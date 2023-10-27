12bad7fe1b19e232a94dc76d2afdaeaa06ada991
Artyom Pionov <pionovartem11@gmail.com>
2023-10-27 13:21:09 +0100

    New Commit: Tests and App is fixed and working.

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,4 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+-- noinspection SqlDialectInspectionForFile
+INSERT INTO SPEND (ID, CHAT_ID, SPEND) VALUES (1, 225, 3000);
+INSERT INTO INCOMES (ID, CHAT_ID, INCOME) VALUES (1, 225, 5500);
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,1 +1,3 @@
-INSERT INTO INCOME (ID, CHAT_ID, SPEND) VALUES (2, 228, 420);
+-- noinspection SqlNoDataSourceInspectionForFile
+-- noinspection SqlDialectInspectionForFile
+INSERT INTO INCOMES (ID, CHAT_ID, SPEND) VALUES (2, 228, 420);
